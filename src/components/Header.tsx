e70555a20ab6c82edc00ea14837031a093b47087
SujinRo <shtnwls1111@ewhain.net>
2023-06-08 04:58:23 +0900

    feat: logo 누르면 home화면에 가도록

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import styles from '../styles/Header.module.css';
 import Logo from '../../public/img/Logo.svg';
+import Link from 'next/link';
 
 export default function Header() {
     return(
         <div className = {styles.HeaderPage}>
+            <Link href= '/'>
             <Logo className = {styles.logo}/>
+            </Link>
             <div className = {styles.buttons}>
             <button className = {styles.registerBtn}>회원가입</button>
             </div>
