2e701663db113a3a56a649f83a32580e9e5e0f0a
SujinRo <shtnwls1111@ewhain.net>
2023-06-12 04:28:32 +0900

    feat: 같은 score일 경우 rank가 같도록

--- src/pages/demoResult/index.tsx
+++ src/pages/demoResult/index.tsx
@@ -4,6 +4,8 @@ import groupData from '../../jsons/groupData.json';
 
 export default function demoResult(){
     const groups = groupData.groups;
+    const rank = [1,2,3,4,5];
+    let newRank;
 
     groups.sort(function compare(a,b){
         return b.score-a.score;
@@ -13,26 +15,34 @@ export default function demoResult(){
         <div className={styles.demoPage}> 
             <Header/>
             <div className={styles.titleText}>데모데이 투표결과</div>
-            {groups.map((group, number) => (
+            {groups.map((group, index) =>{
+                newRank = rank[index];
+
+                if(index !=0 && 
+                    groups[index].score === groups[index-1].score){
+                        rank[index] = rank[index-1];
+                        newRank = rank[index-1];
+                    }
+                return(
                 <>
-                    {number == 0 
+                    {newRank == 1
                     ?
                     <div className={styles.firstLongBox}>
-                    <div className={styles.firstNumberBox}>{number = number + 1}</div>
+                    <div className={styles.firstNumberBox}>{newRank}</div>
                     <div className={styles.resultNameText}>{group.name}</div>
                     <div className={styles.resultDetailText}>{group.detail}</div>
                     <div className={styles.scoreText}>{group.score}</div>
                     </div>
                     : 
                     <div className={styles.longBox}>
-                    <div className={styles.numberBox}>{number = number + 1}</div>
+                    <div className={styles.numberBox}>{newRank}</div>
                     <div className={styles.resultNameText}>{group.name}</div>
                     <div className={styles.resultDetailText}>{group.detail}</div>
                     <div className={styles.scoreText}>{group.score}</div>
                     </div>
                     }
                 </>
-            ))}
+            )})}
         </div>
     )
 }
