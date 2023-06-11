03f79980e3d44e546c5ffa789d1c923757347a85
SujinRo <shtnwls1111@ewhain.net>
2023-06-12 00:53:35 +0900

    feat: demoResult 페이지 결과 순서 번호 나오게

--- src/pages/demoResult/index.tsx
+++ src/pages/demoResult/index.tsx
@@ -1,7 +1,6 @@
 import styles from '../../styles/Demo.module.css';
 import Header from '@/components/Header';
 import groupData from '../../jsons/groupData.json';
-import { useState } from 'react';
 
 export default function demoResult(){
     const groups = groupData.groups;
@@ -14,9 +13,9 @@ export default function demoResult(){
         <div className={styles.demoPage}> 
             <Header/>
             <div className={styles.titleText}>데모데이 투표결과</div>
-            {groups.map((group) => (
+            {groups.map((group, number) => (
                 <div className={styles.longBox}>
-                    <div className={styles.numberBox}></div>
+                    <div className={styles.numberBox}>{number = number + 1}</div>
                     <div className={styles.resultNameText}>{group.name}</div>
                     <div className={styles.resultDetailText}>{group.detail}</div>
                     <div className={styles.scoreText}>{group.score}</div>
