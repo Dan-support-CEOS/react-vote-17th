75484dc96e3639a8306508bf7e2614a92d8b4094
SujinRo <shtnwls1111@ewhain.net>
2023-06-15 02:14:00 +0900

    feat: demoVote 선택한 data 표시 + box위치 정렬

--- src/components/demoVote.tsx
+++ src/components/demoVote.tsx
@@ -8,11 +8,13 @@ interface demoVoteProps{
 
 export default function demoVote({groups}: demoVoteProps){
     const [clickIndex,setClickIndex] = useState(5);
+    const firstGroup = groups.filter(group => group.id < 2);
+    const secondGroup = groups.filter(group => group.id >1);
 
     return(
         <div className={styles.componentPage}>
             <div className={styles.componentPart}>
-            {groups.map((group) => (
+            {firstGroup.map((group) => (
             <div onClick={()=>setClickIndex(group.id)}>
                 {clickIndex === group.id?(
                     <div className={styles.clickedBox}> 
@@ -31,7 +33,28 @@ export default function demoVote({groups}: demoVoteProps){
                 )}
             </div>
             ))}
+        </div>
+        <div className={styles.componentPart}>
+            {secondGroup.map((group) => (
+            <div onClick={()=>setClickIndex(group.id)}>
+                {clickIndex === group.id?(
+                    <div className={styles.clickedBox}> 
+                    <div className={styles.textBox}>
+                    <div className={styles.nameText}>{group.name}</div>
+                    <div className={styles.detailText}>{group.detail}</div>
+                    </div>
+                    </div>
+                ):(
+                    <div className={styles.box}> 
+                    <div className={styles.textBox}>
+                    <div className={styles.nameText}>{group.name}</div>
+                    <div className={styles.detailText}>{group.detail}</div>
+                    </div>  
+                    </div>
+                )}
             </div>
+            ))}
+        </div>
         </div>
     )
 }
