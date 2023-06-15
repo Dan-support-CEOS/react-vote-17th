cdd5046597399d44648b93829c5dc636a29b1b07
SujinRo <shtnwls1111@ewhain.net>
2023-06-16 02:50:44 +0900

    feat: function 통해 code 줄임

--- src/components/demoVote.tsx
+++ src/components/demoVote.tsx
@@ -11,6 +11,7 @@ export default function demoVote({groups}: demoVoteProps){
     const secondGroup = groups.filter(group => group.id >1);
     const [clickIndex,setClickIndex] = useState(5);
     
+    //click유무에 따라 alert
     function onVote(){
         if(clickIndex == 5){
             alert('투표할 팀을 선택해주세요');
@@ -20,50 +21,41 @@ export default function demoVote({groups}: demoVoteProps){
         }
     }
 
+    //click된 box는 border 생기도록 
+    function Box({groups}: demoVoteProps){
+        return(
+            <>
+            {groups.map((group) => (
+            <div onClick={()=>setClickIndex(group.id)}>
+                {clickIndex === group.id?(
+                    <div className={styles.clickedBox}> 
+                    <div className={styles.textBox}>
+                    <div className={styles.nameText}>{group.name}</div>
+                    <div className={styles.detailText}>{group.detail}</div>
+                    </div>
+                    </div>
+                ):(
+                    <div className={styles.box}> 
+                    <div className={styles.textBox}>
+                    <div className={styles.nameText}>{group.name}</div>
+                    <div className={styles.detailText}>{group.detail}</div>
+                    </div>  
+                    </div>
+                )}
+            </div>
+            ))}
+            </>
+        )
+    }
+
     return(
         <>
         <div className={styles.componentPage}>
             <div className={styles.componentPart}>
-            {firstGroup.map((group) => (
-            <div onClick={()=>setClickIndex(group.id)}>
-                {clickIndex === group.id?(
-                    <div className={styles.clickedBox}> 
-                    <div className={styles.textBox}>
-                    <div className={styles.nameText}>{group.name}</div>
-                    <div className={styles.detailText}>{group.detail}</div>
-                    </div>
-                    </div>
-                ):(
-                    <div className={styles.box}> 
-                    <div className={styles.textBox}>
-                    <div className={styles.nameText}>{group.name}</div>
-                    <div className={styles.detailText}>{group.detail}</div>
-                    </div>  
-                    </div>
-                )}
-            </div>
-            ))}
+                <Box groups={firstGroup}/>
         </div>
         <div className={styles.componentPart}>
-            {secondGroup.map((group) => (
-            <div onClick={()=>setClickIndex(group.id)}>
-                {clickIndex === group.id?(
-                    <div className={styles.clickedBox}> 
-                    <div className={styles.textBox}>
-                    <div className={styles.nameText}>{group.name}</div>
-                    <div className={styles.detailText}>{group.detail}</div>
-                    </div>
-                    </div>
-                ):(
-                    <div className={styles.box}> 
-                    <div className={styles.textBox}>
-                    <div className={styles.nameText}>{group.name}</div>
-                    <div className={styles.detailText}>{group.detail}</div>
-                    </div>  
-                    </div>
-                )}
-            </div>
-            ))}
+            <Box groups={secondGroup}/>
         </div>
         </div>
         <button className={styles.voteBtn} onClick={()=>{onVote()}}>투표하기</button>
