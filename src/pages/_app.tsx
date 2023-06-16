195981b970a37a3f8b1867c183c7682ac1bbd0ad
Yoojin <yoojin@Yoojinui-MacBookPro.local>
2023-06-17 03:24:06 +0900

    feat: QueryClient 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  {
+    /*<QueryClientProvider client={queryClient}>*/
+  }
   return <Component {...pageProps} />;
+  {
+    /*</QueryClientProvider>;*/
+  }
 }
