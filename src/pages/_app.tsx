2f0177e67575b7b3a101637b65937da40eb3bb77
Yoojin <yoojin@Yoojinui-MacBookPro.local>
2023-06-17 03:32:58 +0900

    feat: RecoilRoot 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,16 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
-  {
-    /*<QueryClientProvider client={queryClient}>*/
-  }
-  return <Component {...pageProps} />;
-  {
-    /*</QueryClientProvider>;*/
-  }
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
