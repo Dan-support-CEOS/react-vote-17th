de96d671281a85178fce6f3ef168fa85007567e2
SujinRo <shtnwls1111@ewhain.net>
2023-06-27 05:02:05 +0900

    feat: homePage css 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,22 +7,15 @@ export default function Home() {
     <div className={styles.homePage}>
       <Header />
       <div className={styles.part}>
-        <div className={styles.titleTextPart}>파트장 투표</div>
-        <div className={styles.partDevide}>
-          <div className={styles.partText}>FE</div>
-          <div className={styles.partText}>BE</div>
-        </div>
-        <div className={styles.partDevide}>
-          <button className={styles.voteBtnPart}>투표하기</button>
-          <button className={styles.voteBtnPart}>투표하기</button>
-        </div>
+        <div className={styles.titleText}>파트장 투표</div>
+        <button className={styles.voteBtn}>투표하기</button>
         <button className={styles.resultBtn}>결과보기</button>
       </div>
       <hr className={styles.line} />
       <div className={styles.part}>
-        <div className={styles.titleTextDemo}>데모데이 투표</div>
+        <div className={styles.titleText}>데모데이 투표</div>
         <Link href="/vote/demo-day">
-          <button className={styles.voteBtnDemo}>투표하기</button>
+          <button className={styles.voteBtn}>투표하기</button>
         </Link>
         <Link href="/result/demo-day">
           <button className={styles.resultBtn}>결과보기</button>
