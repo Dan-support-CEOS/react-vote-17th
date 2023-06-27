24a6a5735568f5078ce5202448f556abd95e373e
SujinRo <shtnwls1111@ewhain.net>
2023-06-27 18:57:00 +0900

    feat: demoVote, demoResult api 추가

--- src/apis/vote.ts
+++ src/apis/vote.ts
@@ -1,5 +1,6 @@
 import client from './client';
 
+//파트장 투표
 export const votePartLeader = async (info: any) => {
   const response = await client.post(
     '/votes/candidates/',
@@ -16,8 +17,35 @@ export const votePartLeader = async (info: any) => {
   return response.data;
 };
 
+//파트장 투표 중복 제한
 export const checkPartLeaderVoteAuthority = async (accessToken: string) => {
   const response = await client.post('/votes/candidates/authority/', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return response.data;
+};
+
+//데모데이 투표
+export const demoDayVote = async (info: any) => {
+  const response = await client.post(
+    '/votes/teams/',
+    {
+      tname: info.tname,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${info.accessToken}`,
+      },
+    },
+  );
+  return response.data;
+};
+
+//데모데이 투표 중복 제한
+export const demoDayAuthority = async (accessToken: any) => {
+  const response = await client.post('/votes/teams/authority/', {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
--- src/apis/voteResult.ts
+++ src/apis/voteResult.ts
@@ -1,6 +1,13 @@
 import client from './client';
 
+//파트장 결과
 export const getPartLeaderResult = async (part: string) => {
   const response = await client.get(`/votes/candidates/${part}/`);
+  return response.data;
+};
+
+//데모데이 결과
+export const getDemoDayResult = async () => {
+  const response = await client.get('/votes/teams/result/');
   return response.data;
 };
