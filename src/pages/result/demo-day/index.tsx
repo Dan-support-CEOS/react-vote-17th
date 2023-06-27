73c4d0f3c71308988f773bc084fc245a02613928
SujinRo <shtnwls1111@ewhain.net>
2023-06-27 19:04:16 +0900

    feat: demoResult 백에서 값을 받아와서 보여줌

--- src/pages/result/demo-day/index.tsx
+++ src/pages/result/demo-day/index.tsx
@@ -1,47 +1,58 @@
-import styles from '../../../styles/DemoDayPage.module.css';
+import { getDemoDayResult } from '@/apis/voteResult';
+import styles from '../../../styles/Demo.module.css';
 import Header from '@/components/Header';
-import groupData from '../../../jsons/groupData.json';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function DemoDayResultPage() {
-  const groups = groupData.groups;
+  const [groups, setGroups] = useState<any[]>([]);
+
+  const { data: demoDayResult } = useQuery(
+    ['demo-day-result'],
+    getDemoDayResult,
+    {
+      onSuccess: data => {
+        console.log(data);
+        setGroups(data);
+      },
+    },
+  );
+
   const rank = [1, 2, 3, 4, 5];
   let newRank;
-
-  groups.sort(function compare(a, b) {
-    return b.score - a.score;
-  });
 
   return (
     <div className={styles.demoPage}>
       <Header />
       <div className={styles.titleText}>데모데이 투표결과</div>
-      {groups.map((group, index) => {
-        newRank = rank[index];
+      {groups &&
+        groups.map((group, index) => {
+          newRank = rank[index];
 
-        if (index != 0 && groups[index].score === groups[index - 1].score) {
-          rank[index] = rank[index - 1];
-          newRank = rank[index - 1];
-        }
-        return (
-          <>
-            {newRank == 1 ? (
-              <div className={styles.firstLongBox}>
-                <div className={styles.firstNumberBox}>{newRank}</div>
-                <div className={styles.resultNameText}>{group.name}</div>
-                <div className={styles.resultDetailText}>{group.detail}</div>
-                <div className={styles.scoreText}>{group.score}</div>
-              </div>
-            ) : (
-              <div className={styles.longBox}>
-                <div className={styles.numberBox}>{newRank}</div>
-                <div className={styles.resultNameText}>{group.name}</div>
-                <div className={styles.resultDetailText}>{group.detail}</div>
-                <div className={styles.scoreText}>{group.score}</div>
-              </div>
-            )}
-          </>
-        );
-      })}
+          if (index != 0 && groups[index].count === groups[index - 1].count) {
+            rank[index] = rank[index - 1];
+            newRank = rank[index - 1];
+          }
+          return (
+            <>
+              {newRank == 1 ? (
+                <div className={styles.firstLongBox}>
+                  <div className={styles.firstNumberBox}>{newRank}</div>
+                  <div className={styles.resultNameText}>{group.tname}</div>
+                  <div className={styles.resultDetailText}>{group.detail}</div>
+                  <div className={styles.scoreText}>{group.count}</div>
+                </div>
+              ) : (
+                <div className={styles.longBox}>
+                  <div className={styles.numberBox}>{newRank}</div>
+                  <div className={styles.resultNameText}>{group.tname}</div>
+                  <div className={styles.resultDetailText}>{group.detail}</div>
+                  <div className={styles.scoreText}>{group.count}</div>
+                </div>
+              )}
+            </>
+          );
+        })}
     </div>
   );
 }
