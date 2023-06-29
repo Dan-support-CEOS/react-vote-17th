2275acb08117b8006382950882b4d51408543e5e
paya17 <yjshin1003@naver.com>
2023-06-29 16:23:02 +0900

    fix: title 위치 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </RecoilRoot>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>CEOS VOTE</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
   );
 }
