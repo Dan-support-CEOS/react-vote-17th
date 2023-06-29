7d3ac759c657c132933fbf5d5b939d42f335e37d
SujinRo <shtnwls1111@ewhain.net>
2023-06-30 01:35:53 +0900

    feat: json삭제

--- src/pages/vote/demo-day/index.tsx
+++ src/pages/vote/demo-day/index.tsx
@@ -1,9 +1,34 @@
-import groupData from '../../../jsons/groupData.json';
 import VoteBox from '../../../components/DemoDayVotePage/DemoVote';
 import styles from '../../../styles/DemoDayPage.module.css';
 
 export default function DemoDayVotePage() {
-  const group = groupData.groups;
+  const group = [
+    {
+      tname: '댄서포트',
+      detail: '댄서들을 위한 통합 플랫폼',
+      id: 0,
+    },
+    {
+      tname: 'Hooking',
+      detail: 'SNS 마케팅 문구 레퍼런스 플랫폼',
+      id: 1,
+    },
+    {
+      tname: 'Repick',
+      detail: '2040 남성들을 위한 맞춤형 중고 패션 구독 서비스',
+      id: 2,
+    },
+    {
+      tname: '바리바리',
+      detail: 'SNS 마케팅 문구 레퍼런스 플랫폼',
+      id: 3,
+    },
+    {
+      tname: 'TherapEase',
+      detail: '심리상담이 원활하게 이루어지기 위한 업무관리 플랫폼',
+      id: 4,
+    },
+  ];
 
   return (
     <div className={styles.demoPage}>
